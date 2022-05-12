2fd402bb7a4da5c88112382d6d3142ee52600dc7
mariamaitbella <78501823+mariamaitbella@users.noreply.github.com>
2022-05-12 08:08:50 +0100

    Add project files

---
+++ App.js
@@ -0,0 +1,8 @@
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+
+export default function App() {
+  return <WelcomeScreen />;
+}
